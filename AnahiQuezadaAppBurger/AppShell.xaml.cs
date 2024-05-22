116b4d38c05967ff6322704fa7178106d7242fdc
cisco <cisco@UPOAULA10312>
2024-05-22 11:16:51 -0500

    Agregar archivos de proyecto.

---
+++ AnahiQuezadaAppBurger/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AnahiQuezadaAppBurger
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AnahiQuezadaAppBurger/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AnahiQuezadaAppBurger
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
